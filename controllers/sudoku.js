a768e0b1894f235aefd90eefbaf6498e3b9b6759
Deniz89 <ozandozel@gmail.com>
2020-11-08 00:55:48 +0300

    Db related methods removed for sudoku.

--- controllers/sudoku.js
+++ controllers/sudoku.js
@@ -2,16 +2,12 @@ const {success, unexpected_err} = require('../config').RESPONSE_STATUS
 
 const {getToken} = require('../utils/tokenGenerator')
 
-// sudoku methods
-const {upsertGame, getGame} = require('../db/sudoku')
 const {initiateGame} = require('../games/sudoku/engine')
 
 const initiate = async (req, res) => {
   const game = initiateGame(req.body.difficulty)
 
   const token = getToken()
-
-  // const result = await upsertGame({...game, token})
 
   if(game && token) {
     res.status(200).send({
@@ -30,23 +26,6 @@ const initiate = async (req, res) => {
   }
 }
 
-const get = async (req, res) => {
-  let result = await getGame(req.body.token)
-
-  if(result.err) {
-    res.status(500).send({
-      status: unexpected_err,
-      message: JSON.stringify(result.err)
-    })
-  } else {
-    res.status(200).send({
-      status: success,
-      result
-    })
-  }
-}
-
 module.exports = {
-  initiate,
-  get
+  initiate
 }
